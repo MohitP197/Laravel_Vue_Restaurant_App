ab1c781bec40c2105aa917bcd8f307782e2f7b38
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-01-07 21:24:58 +0545

    Added frontend and added images with categories controller

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
