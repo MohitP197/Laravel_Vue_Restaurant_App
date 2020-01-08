5366099430e30ca8fb9a476b3c16a5fece19e383
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-01-08 18:57:45 +0545

    Updated admin categories  page

--- resources/views/Admin/Categories/index.blade.php
+++ resources/views/Admin/Categories/index.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 <h1>Categories</h1>
 {{-- {{{$categories}}} is passed as an array of categories --}}
-<category-manager :categories="{{$categories}}"></category-manager>
+<category-manager :initial-categories="{{$categories}}"></category-manager>
 @endsection
